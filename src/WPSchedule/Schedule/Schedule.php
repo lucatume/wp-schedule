04897b4171f24b7db7d0835cae960f4ba91bd395
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-12-05 14:07:17 +0100

    first passing tests of something real

--- src/WPSchedule/Schedule/Schedule.php
+++ src/WPSchedule/Schedule/Schedule.php
@@ -2,5 +2,7 @@
 
 
 	class WPSchedule_Schedule_Schedule extends WPSchedule_Schedule_AbstractSchedule {
-
+        public function isScheduled($hook, $args = null){
+            return wp_next_scheduled( $hook, $args );            
+        }
 	}
--- tests/unit/WPSchedule_Schedule_ScheduleTest.php
+++ tests/unit/WPSchedule_Schedule_ScheduleTest.php
@@ -1,31 +1,92 @@
 <?php
+use tad\FunctionMocker\FunctionMocker;
 
+class WPSchedule_Schedule_ScheduleTest extends \PHPUnit_Framework_TestCase
+{
+    
+    /**
+     * @var WPSchedule_Schedule_Schedule
+     */
+    protected $sut;
+    
+    /**
+     * @var string
+     */
+    protected $sutClass = 'WPSchedule_Schedule_Schedule';
+    
+    protected function setUp()
+    {
+        
+        // first
+        FunctionMocker::setUp();
+        $this->sut = new WPSchedule_Schedule_Schedule();
+    }
+    
+    protected function tearDown()
+    {
+        
+        // last
+        FunctionMocker::tearDown();
+    }
+    
+    /**
+     * @test
+     * it should be initializable
+     */
+    public function it_should_be_initializable()
+    {
+        $this->assertInstanceOf($this->sutClass, $this->sut);
+    }
+    
+    /**
+     * @test
+     * it should return false if wp_next_scheduled returns false
+     */
+    public function it_should_return_false_if_wp_next_scheduled_returns_false()
+    {
+        FunctionMocker::replace('wp_next_scheduled', false);
+        
+        $this->assertFalse($this->sut->isScheduled('some_hook'));
+    }
+    
+    /**
+     * @test
+     * it should return true if wp_next_scheduled returns true
+     */
+    public function it_should_return_true_if_wp_next_scheduled_returns_true()
+    {
+        FunctionMocker::replace('wp_next_scheduled', true);
+        
+        $this->assertTrue($this->sut->isScheduled('some_hook'));
+    }
+    
+    /**
+     * @test
+     * it should return false if wp_next_scheduled returns false with args
+     */
+    public function it_should_return_false_if_wp_next_scheduled_returns_false_with_args()
+    {
+        FunctionMocker::replace('wp_next_scheduled', function ($hook, $args)
+        {
+            return $args !== ['some', 'args'];
+        });
+        
+        $this->assertFalse($this->sut->isScheduled('some_hook', ['some', 'args']));
+        $this->assertTrue($this->sut->isScheduled('some_hook'));
+    }
 
-	class WPSchedule_Schedule_ScheduleTest extends \PHPUnit_Framework_TestCase {
-
-		/**
-		 * @var WPSchedule_Schedule_Schedule
-		 */
-		protected $sut;
-
-		/**
-		 * @var string
-		 */
-		protected $sutClass = 'WPSchedule_Schedule_Schedule';
-
-		protected function setUp() {
-			$this->sut = new WPSchedule_Schedule_Schedule();
-		}
-
-		protected function tearDown() {
-		}
-
-		/**
-		 * @test
-		 * it should be initializable
-		 */
-		public function it_should_be_initializable() {
-			$this->assertInstanceOf( $this->sutClass, $this->sut );
-		}
-
-	}
+        /**
+         * @test
+         * it should return true if wp_next_scheduled returns true with args
+         */
+        public function it_should_return_true_if_wp_next_scheduled_returns_true_with_args()
+        {
+	        FunctionMocker::replace('wp_next_scheduled', function ($hook, $args)
+	        {
+	            return $args === ['some', 'args'];
+	        });
+        
+	        $this->assertTrue($this->sut->isScheduled('some_hook', ['some', 'args']));
+	        $this->assertFalse($this->sut->isScheduled('some_hook'));
+        }
+}
